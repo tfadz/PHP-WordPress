121020ceb56eeb5c1a407e578e7bfa95d93fe7eb
Terry Fadz <alaterry@gmail.com>
2019-06-27 16:50:49 -0500

    Added if else example

---
+++ if-else/index.php
@@ -0,0 +1,8 @@
+<?php if (! is_page_template('page-partner.php') ) { ?>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/destination.svg" />
+
+
+<?php } else { ?>
+    <img src="<?php bloginfo('template_directory'); ?>/assets/images/address-dark.svg" />
+
+<?php } ?>
