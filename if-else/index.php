71c9b0031c964424f578bac674bb6cbecd5f944e
Terry Fadz <alaterry@gmail.com>
2019-08-20 13:11:07 -0500

    updated if else

--- if-else/index.php
+++ if-else/index.php
@@ -1,8 +1,17 @@
-<?php if (! is_page_template('page-partner.php') ) { ?>
-    <img src="<?php bloginfo('template_directory'); ?>/assets/images/destination.svg" />
+Example 1
 
+<?php if (! is_page_template('page-partner.php') ) { ?>
+  <img src="<?php bloginfo('template_directory'); ?>/assets/images/destination.svg" />
 
 <?php } else { ?>
-    <img src="<?php bloginfo('template_directory'); ?>/assets/images/address-dark.svg" />
+  <img src="<?php bloginfo('template_directory'); ?>/assets/images/address-dark.svg" />
 
 <?php } ?>
+
+Example 2
+
+<?php if($eyebrow_txt) : ?>
+<span><?php echo $hero1['eyebrow'] ?></span>
+<?php elseif($eyebrow_img) : ?>
+<img width="460" class="w-50 w-md-35 mx-auto" src="<?php echo $eyebrow_img ?>" alt="">
+<?php endif; ?>
