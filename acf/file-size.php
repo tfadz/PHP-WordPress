6db3307c987c7595c4b358404958b7c3b41fc4b9
Terry Fadz <terry@glantz.net>
2019-12-17 12:05:43 -0600

    Added url file acf

--- acf/file-size.php
+++ acf/file-size.php
@@ -1,34 +1,34 @@
-<section class="pt3 downloads">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                <?php if( have_rows('download_item') ): while ( have_rows('download_item') ) : the_row(); ?>
-                <?php
-                    $title = get_sub_field('title');
-                    $desc = get_sub_field('description');
-                    $pdf = get_sub_field('download_pdf');
-                    $pdf_name = get_sub_field('link_name');
+<!-- ACF File (Using ID) for showing links and file size -->
 
-                    $attachment_id = get_sub_field('download_pdf');
-                    $url = wp_get_attachment_url( $attachment_id );
+<div class="row">
+    <div class="col-lg-12">
+        <?php if( have_rows('download_item') ): while ( have_rows('download_item') ) : the_row(); ?>
+            <?php
+            $title = get_sub_field('title');
+            $desc = get_sub_field('description');
+            $pdf = get_sub_field('download_pdf');
+            $pdf_name = get_sub_field('link_name');
+            $size = 'large'; 
+            $pdf_link = wp_get_attachment_url( $pdf );
 
-                    // part where to get the filesize
-                    $filesize = filesize( get_attached_file( $attachment_id ) );
-                    $filesize = size_format($filesize, 2);
+            $attachment_id = get_sub_field('download_pdf');
+            $url = wp_get_attachment_url( $attachment_id );
 
-                 ?>
-                 <article class="item">
-                     <h2><?php echo $title ?></h2>
-                     <p><?php echo $desc ?></p>
-                     <div class="row">
-                        <div class="col-lg-6 col-sm-12 download"><a href="<?php echo $pdf ?>">Download <?php echo $pdf_name ?> (<?php echo $filesize; ?>)</a></div>
-                        <div class="col-lg-6 col-sm-12 email-pdf"><a href="mailto:?body=<?php echo $pdf ?>">Email this Asset</a></div>
-                     </div>
-                 </article>
+                    // get the filesize
+            $filesize = filesize( get_attached_file( $attachment_id ) );
+            $filesize = size_format($filesize, 2);
 
-    
-                <?php endwhile; endif; ?>
-                </div>
+            ?>
+            <article class="item">
+               <h2><?php echo $title ?></h2>
+               <p><?php echo $desc ?></p>
+               <div class="row">
+                <div class="col-lg-6 col-sm-12 download"><a href="<?php echo esc_html($pdf_link) ?>" target="_blank">Download <?php echo $pdf_name ?> (<?php echo $filesize; ?>)</a></div>
+                <div class="col-lg-6 col-sm-12 email-pdf"><a href="mailto:?body=<?php echo esc_html($pdf_link) ?>">Email this Asset</a></div>
             </div>
-        </div>
-    </section>
+        </article>
+
+        
+    <?php endwhile; endif; ?>
+</div>
+</div>
