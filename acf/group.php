098aea528188af57ea7232d47351ab9a51f484d3
Terry Fadz <terry@glantz.net>
2020-03-03 14:02:24 -0600

    group acf

---
+++ acf/group.php
@@ -0,0 +1,8 @@
+<?php
+  $image = get_sub_field('image');
+  $titles = get_sub_field('title');
+  $title1 = $titles['line_1'];
+  $title2 = $titles['line_2'];
+  $title3 = $titles['line_3'];
+  $size = 'full';
+?>
