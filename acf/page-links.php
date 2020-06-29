8014d8988405181d3e55f0cdd061faebee1ad47b
Terry Fadz <terry@glantz.net>
2020-06-29 08:59:36 -0500

    updated page links

--- acf/page-links.php
+++ acf/page-links.php
@@ -3,3 +3,11 @@
 <?php foreach ( $urls as $urls_item ) { ?>
   <li><a href="<?php echo get_the_permalink($urls_item); ?>" title="<?php echo get_the_title($urls_item); ?>" class="button"><?php echo get_the_title($urls_item); ?></a></li>
 <?php } ?>
+
+<!-- When using Page link and you need to have option for open in new tab -->
+
+<?php
+$link = get_field('link');
+
+?>
+<a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
