7ac9dc090fc0facedc717895b2fc0389bd29e3e3
Terry Fadz <terry@glantz.net>
2020-07-17 10:55:29 -0500

    updated

--- wp-query/index.php
+++ wp-query/index.php
@@ -1,3 +1,5 @@
+Example 1
+
 <?php 
 $args = array(
   'post_type' => 'post',
@@ -22,3 +24,33 @@ if( $query_posts->have_posts() ) : ?>
   <?php endwhile; ?> 
 <?php endif; ?>
 <?php wp_reset_postdata();  ?> 
+
+
+Example 2
+
+<?php if ( $query->have_posts() ) : ?>
+  <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+    <div class="row mb-2 pb-2">   
+      <div class="the-post col-lg-8">
+        <div class="meta">
+          <h3><a href="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a></h3>
+          <div><?php the_excerpt(); ?></div>
+          <div class="d-flex"><?php the_category(); ?> <div class="author"><?php the_author(); ?></div></div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="featured-img">
+          <?php $featured_img_url = get_the_post_thumbnail_url($p->ID,'medium_large'); ?>
+          <img src="<?php echo $featured_img_url ?>" alt="">
+        </div>
+      </div>
+    </div>
+
+  <?php endwhile; ?>
+
+  <?php wp_reset_postdata(); ?>
+
+  <?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <?php endif; ?>
+        
