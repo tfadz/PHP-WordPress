f6d0bf93c8497aef66d29310786955a14e849c61
Terry Fadz <terry@glantz.net>
2020-09-04 10:56:08 -0500

    updated taxonomy

--- wp-query/taxonomy-post.php
+++ wp-query/taxonomy-post.php
@@ -26,5 +26,71 @@ foreach($custom_terms as $custom_term) {
      }
 }
 
-
  ?>
+
+
+<?php 
+
+ // Simply output the tax and children (simple method)
+
+wp_list_categories( array(
+'taxonomy'  => 'consulting_services',
+'title_li' => ''
+) ); 
+?>
+
+
+
+<?php
+
+// To pull the parent taxonomy and then show the children terms (used in CTLGroup website)
+$services = get_terms(
+    array(
+        'taxonomy'   => array( 'consulting_services' ),
+        'hide_empty' => false,
+        'parent' => 0
+    )
+);
+?>
+
+<?php foreach ( $services as $service_term ) { ?>
+  <div class="row service">
+    
+    <div class="col-lg-6 primary">
+       <!-- Parent Term -->
+      <h3> <?php echo esc_html( $service_term->name ); ?></h3> 
+    </div>
+
+    <div class="col-lg-6 secondary">
+      <!-- Get Children of Taxonomy -->
+      <?php
+        $parent_term = $service_term->term_id;
+        $parent_term_tax = $service_term->taxonomy;
+      ?>
+      <?php
+      $child_terms = get_terms( array(
+        'taxonomy' => $parent_term_tax, 
+        'child_of' => $parent_term,
+        'parent' => $parent_term, 
+        'hide_empty' => false,
+      ) );
+      ?>  
+
+      <?php 
+      if ($child_terms) {
+        echo '<ul class="second-level-terms">';
+
+        foreach ($child_terms as $child_term) {
+          $second_term_name = $child_term->name;
+          echo '<li class="second-level-term">'. '<a href="'.get_permalink().'">' .$second_term_name. '</a>'.'</li>';
+        }
+        echo '</ul>';
+      }
+      ?>
+    </div>
+  </div> 
+<?php } ?>
+      
+      
+
+    
