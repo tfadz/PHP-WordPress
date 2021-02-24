352a61b09f2ee6083549b9877e333ee963ffc442
Terry Fadz <terry.fadz@thirdwavellc.com>
2021-02-24 14:43:23 -0600

    added another example to cpt terms

--- wp-query/cpt-category.php
+++ wp-query/cpt-category.php
@@ -3,7 +3,18 @@
 <?php
 $terms = get_the_terms( $post->ID , 'resource_category' );
 foreach ( $terms as $term ) {
-    echo '<h6 class="category">' . $term->name . '</h6>';
+	echo '<h6 class="category">' . $term->name . '</h6>';
 }
+?>
 
-?> 
+// To check for certain values in the category
+
+<?php
+foreach ( $terms1 as $term ) {
+	if ( $term->name == 'No') :
+		echo '';
+	elseif ($term->name == 'Yes') :
+		echo '<h6 class="category">' . 'Virtual' . '</h6>';
+	endif;
+}
+?>
