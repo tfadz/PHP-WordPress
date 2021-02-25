4141be3997161394b9195f98878b351ab3c6ffd8
Terry Fadz <terry.fadz@thirdwavellc.com>
2021-02-25 09:19:43 -0600

    updated shortcode

--- acf/short-code.php
+++ acf/short-code.php
@@ -1,1 +1,6 @@
 <?php do_shortcode(get_field('your-custom-field')); ?>
+
+<?php 
+	$res_fimage = get_field('res_link_url');
+	echo do_shortcode('[beautiful_link_preview  url="' . $res_fimage . '"]') 
+?>
