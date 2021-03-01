a35ac624d999916c1e6545c6379bd37ff7aa3c0f
Terry Fadz <terry.fadz@thirdwavellc.com>
2021-03-01 12:50:46 -0600

    added category example

--- wp-query/category-stuff.php
+++ wp-query/category-stuff.php
@@ -15,3 +15,14 @@ echo '</ul>';
 // Check if post has category
 
 <?php if (has_category('news')) : ?>hello <?php endif; ?>
+
+
+// Query only posts with certain category. Use 'cat';
+
+<?php
+$args = array(
+'post_type' => 'post',
+'posts_per_page' => '-1',
+'orderby' => 'date',
+'cat' => 21, <-- //look up what the category id number is in the Categories list.
+);
