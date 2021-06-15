973c1b29e970749c140e14fe33c338c25c22659a
Terry Fadz <terry.fadz@thirdwavellc.com>
2021-06-15 14:24:52 -0500

    added acf link example

--- acf/page-links.php
+++ acf/page-links.php
@@ -4,6 +4,18 @@
   <li><a href="<?php echo get_the_permalink($urls_item); ?>" title="<?php echo get_the_title($urls_item); ?>" class="button"><?php echo get_the_title($urls_item); ?></a></li>
 <?php } ?>
 
+
+<!-- expose page title of page link acf field -->
+
+<?php if(get_field('location_installs')) : ?>
+  <ul>
+    <?php if(have_rows('location_installs')) : while (have_rows('location_installs')) : the_row(); ?>
+      <?php $post_id = get_sub_field('link', false, false); ?>
+      <li><a href="<?php the_sub_field('link') ?>"><?php echo get_the_title($post_id); ?></a></li>
+    <?php endwhile; endif; ?>
+  </ul>
+<?php endif; ?>
+
 <!-- When using Page link and you need to have option for open in new tab -->
 
 <?php
