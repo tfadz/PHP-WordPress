008f6257b745692bb18701f3af572c2ccd70a44a
Terry Fadz <terry.fadz@thirdwavellc.com>
2022-02-11 10:50:25 -0600

    updated trim text

--- wp-query/trim-text.php
+++ wp-query/trim-text.php
@@ -2,3 +2,7 @@
 $content = get_the_content();
 echo wp_trim_words( $content , '20' );
  ?>
+ 
+<!--  Another way with acf -->
+<?php $pdesc = get_field('portfolio_description') ?>
+<p><?php $pdesc = mb_strimwidth($pdesc, 0, 102, "..."); echo $pdesc; ?></p>
