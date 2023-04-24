5b138f87eff5224df09f0285b7b39b02153ff379
Terry Fadz <tfadz@users.noreply.github.com>
2023-04-24 14:11:25 -0500

    Updated code

--- plugins/facet-get-taxonomy-update-filter.php
+++ plugins/facet-get-taxonomy-update-filter.php
@@ -1,20 +1,26 @@
-<?php 
-	$resourceCats = get_field('exclude_taxonomy'); 
-	$customTax = $resourceCats->name; 
+<?php
+/* Get ACF field taxonomy value and then capture name of the category */
+$resourceGalleryTax = get_field('resources_gallery_tax'); 
+$customTaxName = $resourceGalleryTax->name; 
 ?>
-
+<?php if($resourceGalleryTax) : ?>
 <script>
 jQuery(function($) {
-	window.setTimeout(function() { 
-		
-		$('.facetwp-type-checkboxes .facetwp-checkbox .facetwp-display-value').each(function() {
-			var $terry = $(this).text();
-				if($terry == '<?php echo $customTax ?>') {
+/* 
+	1. First check to see if page is already filtered.
+	2. Set timeout to wait for wp facet to load.
+	3. Trigger event so category checkbox filter (hidden) is clicked. 
+*/
+	if(!window.location.href.indexOf("?") > -1) {
+		window.setTimeout(function() { 
+			$('.facetwp-type-checkboxes .facetwp-checkbox .facetwp-display-value').each(function() {
+				var $captureCheckboxText = $(this).text();
+				if($captureCheckboxText == '<?php echo $customTaxName ?>') {
 					$(this).parent().trigger("click");
 				}
-		});
-
-
-	}, 150);
+			});
+		}, 100);
+	}
 });
 </script>
+<?php endif; ?>
