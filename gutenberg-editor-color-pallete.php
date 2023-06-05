2dd5565d8787a6fde51e5373a739aef8661f0cbf
Terry Fadz <tfadz@users.noreply.github.com>
2023-06-05 10:18:39 -0500

    Create gutenberg-editor-color-pallete
    Added code to show custom palette colors in the gutenberg admin.

---
+++ gutenberg-editor-color-pallete.php
@@ -0,0 +1,41 @@
+function website_colors_setup() {
+  
+	// Editor Color Palette
+	add_theme_support( 'editor-color-palette', array(
+    	array(
+			'name'  => __( 'Black', 'website' ),
+			'slug'  => 'black',
+			'color' => '#131e29',
+		),
+	
+		array(
+			'name'	=> __( 'Red', 'website' ),
+			'slug'	=> 'red',
+			'color'	=> '#E30613',
+		),
+        array(
+			'name'  => __( 'Gray', 'website' ),
+			'slug'  => 'gray',
+			'color'	=> '#3A3E43',
+		),
+        array(
+			'name'  => __( 'Medium Gray', 'website' ),
+			'slug'  => 'medium-gray',
+			'color'	=> '#60666E',
+		),
+		array(
+			'name'  => __( 'Light Gray', 'website' ),
+			'slug'  => 'light-gray',
+			'color'	=> '#DDDFE1',
+		),
+        
+        array(
+			'name'  => __( 'Lightest Gray', 'website' ),
+			'slug'  => 'lightest-gray',
+			'color'	=> '#F6F7F7',
+		),
+        
+    
+	) );
+}
+add_action( 'after_setup_theme', 'website_colors_setup' );
