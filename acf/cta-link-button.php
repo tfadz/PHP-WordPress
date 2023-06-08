7df9dae5d96a17f4bf3814f44c98c9e0c4355665
Terry Fadz <tfadz@users.noreply.github.com>
2023-06-08 15:56:58 -0500

    Update cta-link-button.php

--- acf/cta-link-button.php
+++ acf/cta-link-button.php
@@ -1,9 +1,8 @@
-$cta_btn = get_field('cta_button');
-
-$cta_btn_title = $cta_btn['title'];
-
-$cta_btn_link = $cta_btn['url'];
-
- 
-
-<?php if($cta_btn) : ?> <a class="button  href="<?php echo esc_url( $cta_btn_link ); ?>"><?php echo esc_html( $cta_btn_title ); ?> <i class="far fa-arrow-right"></i></a><?php endif; ?>
+<?php 
+$cta_text_band_btn = get_field('cta_text_band_button');
+if( $cta_text_band_btn ):  // This is important you check otherwise will down page!
+  $cta_text_band_btn_title = $cta_text_band_btn['title'];
+  $cta_text_band_link = $cta_text_band_btn['url'];
+endif;
+?>
+<?php if($cta_text_band_btn) : ?> <a class="button inline sand" href="<?php echo esc_url( $cta_text_band_link ); ?>"><?php echo esc_html( $cta_text_band_btn_title ); ?> <i class="far fa-arrow-right"></i></a><?php endif; ?>
